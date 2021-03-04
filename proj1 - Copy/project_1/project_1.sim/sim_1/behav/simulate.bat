0e851f349792d75c43e467fbd96ed3b5ec709259
cel8473 <36010541+cel8473@users.noreply.github.com>
2021-03-03 20:52:18 -0500

    Delete proj1 - Copy/project_1 directory

--- proj1 - Copy/project_1/project_1.sim/sim_1/behav/simulate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xsim MIPS_tb_behav -key {Behavioral:sim_1:Functional:MIPS_tb} -tclbatch MIPS_tb.tcl -view D:/DSD-II/proj1 - Copy/project_1/microprocessor_tb_behav.wcfg -log simulate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
